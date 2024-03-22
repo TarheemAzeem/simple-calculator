c8c3cdee2a1548a738ad87f0e6c8dbd829e8638c
TarheemAzeem <160678494+TarheemAzeem@users.noreply.github.com>
2024-03-23 00:07:37 +0500

    push my calculator to github

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
     {
         message: "Select one of the operators to perform operation",
         type: "list",
         name: "operator",
         choices: ["Addition", "Subtraction", "Multiplication", "Division"],
-    },
+    }
 ]);
 //conditional statement
 if (answer.operator === "Addition") {
@@ -20,9 +20,10 @@ else if (answer.operator === "Subtraction") {
 else if (answer.operator === "Multiplication") {
     console.log(answer.firstNumber * answer.secondNumber);
 }
-else if (answer.operator === "Division ") {
+else if (answer.operator === "Division") {
     console.log(answer.firstNumber / answer.secondNumber);
 }
 else {
     console.log("Please select valid operator");
 }
+console.log("THE END");
--- index.ts
+++ index.ts
@@ -10,9 +10,8 @@ const answer = await inquirer.prompt([
     type: "list",
     name: "operator",
     choices: ["Addition", "Subtraction", "Multiplication", "Division"],
-  },
-]);
-
+  }
+  ]);
 //conditional statement
 if (answer.operator === "Addition") {
   console.log(answer.firstNumber + answer.secondNumber);
@@ -20,7 +19,7 @@ if (answer.operator === "Addition") {
   console.log(answer.firstNumber - answer.secondNumber)
 } else if (answer.operator === "Multiplication") { 
     console.log(answer.firstNumber * answer.secondNumber)
-} else if (answer.operator === "Division ") { 
+} else if (answer.operator === "Division") { 
   console.log(answer.firstNumber / answer.secondNumber)
 } else {
   console.log("Please select valid operator")
